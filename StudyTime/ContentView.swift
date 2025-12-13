648120700da8ecedc30fa17b786b89528bee631d
舛水 <159630888+inkrt@users.noreply.github.com>
2025-12-13 23:31:07 +0900

    Initial Commit

---
+++ StudyTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudyTime
+//
+//  Created by 舛水葵 on 2025/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudyTime/StudyTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyTimeApp.swift
+//  StudyTime
+//
+//  Created by 舛水葵 on 2025/12/13.
+//
+
+import SwiftUI
+
+@main
+struct StudyTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
